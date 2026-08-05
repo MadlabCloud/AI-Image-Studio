368d07949c36af4a1d19dffeb555eef8e0739650
Madlab Cloud <237726820+MadlabCloud@users.noreply.github.com>
2026-08-05 12:18:49 +0200

    Make release tests compatible with Python 3.10

--- tests/test_release.py
+++ tests/test_release.py
@@ -2,7 +2,11 @@ from pathlib import Path
 import hashlib
 import subprocess
 import sys
-import tomllib
+
+try:
+    import tomllib
+except ModuleNotFoundError:  # Python 3.10
+    import tomli as tomllib
 
 ROOT = Path(__file__).resolve().parents[1]
 
